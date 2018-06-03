e683a6c2553701876b6829c8af2fc9c83e494264
Serhii Bilorus <serhii.bilorus@epam.com>
2018-06-03 20:06:09 +0300

    [qa] ...

--- spring_book/SpringiA4_SourceCode/Chapter_02/stereo-autoconfig/src/main/java/soundsystem/CDPlayerConfig.java
+++ spring_book/SpringiA4_SourceCode/Chapter_02/stereo-autoconfig/src/main/java/soundsystem/CDPlayerConfig.java
@@ -3,6 +3,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(value = "soundsystem")
 public class CDPlayerConfig { 
 }
